b6a4dab0f96adf306084561c0e5f247e4109bc53
Oddstr13 <oddstr13@openshell.no>
2011-08-09 02:55:14 +0200

    first commit with actual code

---
+++ src/main/java/no/openshell/oddstr13/minecartmeter/MinecartMeterCommandhandler.java
@@ -0,0 +1,51 @@
+package no.openshell.oddstr13.minecartmeter;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Handler for the /pos sample command.
+ * @author SpaceManiac
+ */
+public class MinecartMeterCommandhandler implements CommandExecutor {
+    private final MinecartMeter plugin;
+
+    public MinecartMeterCommandhandler(MinecartMeter plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] split) {
+        System.out.println("[DEBUG]: command minecartmeter executed");
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+        Player player = (Player) sender;
+        player.sendMessage("/minecartmeter");
+        return true;
+/*
+        if (split.length == 0) {
+            Location location = player.getLocation();
+            player.sendMessage("You are currently at " + location.getX() +"," + location.getY() + "," + location.getZ() +
+                    " with " + location.getYaw() + " yaw and " + location.getPitch() + " pitch");
+            return true;
+        } else if (split.length == 3) {
+            try {
+                double x = Double.parseDouble(split[0]);
+                double y = Double.parseDouble(split[1]);
+                double z = Double.parseDouble(split[2]);
+
+                player.teleportTo(new Location(player.getWorld(), x, y, z));
+            } catch (NumberFormatException ex) {
+                player.sendMessage("Given location is invalid");
+            }
+            return true;
+        } else {
+            return false;
+        }
+*/
+    }
+}
---
+++ src/main/java/no/openshell/oddstr13/minecartmeter/MinecartMeterListener.java
@@ -0,0 +1,37 @@
+package no.openshell.oddstr13.minecartmeter;
+
+import org.bukkit.Location;
+import org.bukkit.event.vehicle.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Vehicle;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
+
+/**
+ * Handle events for all Player related events
+ * @author Dinnerbone
+ */
+public class MinecartMeterListener extends VehicleListener {
+    private final MinecartMeter plugin;
+
+    public MinecartMeterListener(MinecartMeter instance) {
+        plugin = instance;
+    }
+
+    @Override
+    public void onVehicleEnter(VehicleEnterEvent event) {
+        Entity entity = event.getEntered();
+        Vehicle vehicle = event.getVehicle();
+        if (vehicle instanceof Minecart) {
+            Minecart cart = (Minecart) vehicle;
+            if (entity instanceof Player) {
+                Player player = (Player) entity;
+                Location l = cart.getLocation();
+                String msg = "entered minecart with entityId " + cart.getEntityId() + 
+                  " at location " + l.getWorld().getName() + "," + l.getX() + "," + l.getY() + "," + l.getZ();
+                player.sendmessage("[DEBUG]: You " + msg);
+                System.out.println("[DEBUG]: " + player.getDisplayName() + "(" + player.getName() + ") " + msg);
+            }
+        }
+    }
+}
